bc5fbfe9b0bf787b6b7fe094b80591bfdf7800ab
Yoosuf Sayyid <cena26657@gmail.com>
2022-09-25 14:07:06 +0500

    test

--- pages/index.js
+++ pages/index.js
@@ -16,26 +16,4 @@ export default function Home() {
 }
 
 
-h) HelpDeskStaff (extends Staff super class)
-HelpDeskStaff ( ) - This method is the constructor method for the class and is run every time an object of this class is created.
-addMember ( ) - This method adds a new member to the members list by creating new member object.
-addService ( ) - This method adds a new service to the services list by creating new service object.
-removeMember ( ) - This method removes a member from the members list.
-removeService ( ) - This method removes a service from the services list.
-applyDiscount ( ) - This method applies a discount for the members.
-createInvoice ( ) - This method creates and displays an invoice for a search result.
-calSalary ( ) - This method calculates the salary of the staff member.
-
-i) GymReceptionistStaff (extends Staff super class)
-GymReceptionistStaff ( ) - This method is the constructor method for the class and is run every time an object of this class is created.
-addMember ( ) - This method adds a new member to the members list by creating new member object.
-removeMember ( ) - This method removes a member from the members list.
-calSalary ( ) - This method calculates the salary of the gym receptionist.
-
-j) InstructorStaff (extends Staff super class)
-InstructorStaff ( ) - This method is the constructor method for the class and is run every time an object of this class is created.
-setClass ( ) - This method assigns the specific class service to the instructor.
-getClass( ) - This method returns the class service assigned to the instructor.
-calSalary ( ) - This method calculates the salary of the instructor.
-
 
